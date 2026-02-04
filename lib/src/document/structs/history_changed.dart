c5732483df771d624981022bdb15b354d0761488
Whiterzi <catwhiter@gmail.com>
2026-02-04 16:31:57 +0800

    [Quire] #24027: Fix positioning cursor by diff when undo/redo

--- lib/src/document/structs/history_changed.dart
+++ lib/src/document/structs/history_changed.dart
@@ -4,9 +4,11 @@ import 'package:flutter/foundation.dart' show immutable;
 class HistoryChanged {
   const HistoryChanged(
     this.changed,
-    this.len,
+    this.diff,
+    this.retains,
   );
 
   final bool changed;
-  final int len;
+  final int diff;
+  final int retains;
 }
